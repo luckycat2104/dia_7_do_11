d5738635638c1946445622963574ff96b980366c
Mello Sonza <130166351+luckycat2104@users.noreply.github.com>
2023-11-14 08:13:38 -0300

    Add files via upload

---
+++ dia 7 do 11/script.js
@@ -0,0 +1,74 @@
+var valorInicial = 100
+
+function botaoProduto1 (){
+    //let valorInicial = 100
+    var cafep = document.getElementById('cafe')
+    cafep.innerText = `Cafés restantes: ${valorInicial}`
+    if ( valorInicial >= 1){
+        let resultado = valorInicial --
+        alert ("Seu pedido foi realizado :)")
+    }
+   else{
+    alert ("Produto indisponivel :(")
+   }
+}
+
+var valorInicial2 = 100
+
+function botaoProduto2 (){
+    //let valorInicial = 100
+    var cafep2 = document.getElementById('cafe2')
+    cafep2.innerText = `Cafés restantes: ${valorInicial2}`
+    if ( valorInicial2 >= 1){
+        let resultado = valorInicial2 --
+        alert ("Seu pedido foi realizado :)")
+    }
+   else{
+    alert ("Produto indisponivel :(")
+   }
+}
+
+var valorInicial3 = 100
+
+function botaoProduto3 (){
+    //let valorInicial = 100
+    var cafep3 = document.getElementById('cafe3')
+    cafep3.innerText = `Cafés restantes: ${valorInicial3}`
+    if ( valorInicial3 >= 1){
+        let resultado = valorInicial3 --
+        alert ("Seu pedido foi realizado :)")
+    }
+   else{
+    alert ("Produto indisponivel :(")
+   }
+}
+
+var valorInicial4 = 100
+
+function botaoProduto4 (){
+    //let valorInicial = 100
+    var cafep = document.getElementById('cafe4')
+    cafep4.innerText = `Cafés restantes: ${valorInicial4}`
+    if ( valorInicial4 >= 1){
+        let resultado = valorInicial4 --
+        alert ("Seu pedido foi realizado :)")
+    }
+   else{
+    alert ("Produto indisponivel :(")
+   }
+}
+
+var valorInicial5 = 100
+
+function botaoProduto5 (){
+    //let valorInicial = 100
+    var cafep5 = document.getElementById('cafe5')
+    cafep5.innerText = `Cafés restantes: ${valorInicial5}`
+    if ( valorInicial5 >= 1){
+        let resultado = valorInicial5 --
+        alert ("Seu pedido foi realizado :)")
+    }
+   else{
+    alert ("Produto indisponivel :(")
+   }
+}
